6c331ea0e9bab0a1adfefaeae25e5869944dee02
Mitansh-Patel-24120033 <mitansh.patel@iitgn.ac.in>
2025-04-17 16:02:29 +0530

    Add files via upload

---
+++ db_management_system/__init__.py
@@ -0,0 +1,7 @@
+# This file makes the 'database' directory a Python package.
+# It can be left empty.
+
+# You could potentially import key classes here for easier access, e.g.:
+# from .bplustree import BPlusTree
+# from .table import Table
+# from .db_manager import Database
